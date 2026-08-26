5f87cfe5300300e636e6e00851f8feff753e445e
Daniel Leal <daniel@fintoc.com>
2026-08-26 16:03:22 -0400

    fix: rename the local alias breaking the work terminal config

--- dotfiles/layers/work/user/.config/shell/aliases.d/here.sh
+++ dotfiles/layers/work/user/.config/shell/aliases.d/here.sh
@@ -1,10 +1,7 @@
-local_tmux() {
+here() {
   if command tmux has-session 2>/dev/null; then
     command tmux attach-session
   else
     command smug start home --attach
   fi
 }
-
-# `local` is reserved by zsh, so it cannot be declared as a shell function
-alias local=local_tmux
